43e07c7a829719189e995999a51a18692ff85a81
maissacrement <emmariodelar@gmail.com>
2019-10-22 23:50:56 +0200

    Add first boucle

--- src/App.ts
+++ src/App.ts
@@ -2,11 +2,9 @@ import BruteForceProvider from './providers/BruteForceProvider';
 
 const main = () => {
   const bruteForceProvider = new BruteForceProvider();
-  bruteForceProvider.init();
 
-  const password = bruteForceProvider.getCurrentPassword();
-  console.log('Voici mon pass', password);
-  bruteForceProvider.bruteForce(password)
+  bruteForceProvider.init();
+  bruteForceProvider.bruteForce();
 };
 
 main();
--- src/providers/BruteForceProvider.ts
+++ src/providers/BruteForceProvider.ts
@@ -9,42 +9,55 @@ export default class BruteForceProvider {
   /** Main Function **/
 
   public init() {
-    const firstLetterOfOmega = this.omega[0];
+    const firstLetterInOmega = this.omega[0];
 
     for(let i=0;i<this.hashCount;i++) {
-      this.passwordTable.push(fistLetterOfOmega);
+      this.passwordTable.push(firstLetterInOmega);
     }
   }
 
   public getCurrentPassword(): string {
-    console.log(this.passwordTable);
     const newPass = this.passwordTable;
 
     return newPass.join().replace(/[.\,$+]/g, '');
   }
 
-  public bruteForce(password: string): void {
+  public bruteForce() {
+    const passwordLength = this.passwordTable.length - 1;
+    let i = 0;
+
+    while(i < 10) {
+      const password = this.getCurrentPassword();
+      this.chageValueOfPasswordTableIndex(0, passwordLength, i)
+      this._7zTryFindPassword( password );
+
+      console.log(password);
+      i++;
+    }
+  }
+
+
+  private _7zTryFindPassword(password: string): void {
     const tryPasswordBruteForce = spawn('7z', [`${this.target}`, `-P${password}`]);
 
     tryPasswordBruteForce.stdout.on('data', (data: any) => {
-      console.log(`stdout: ${data}`);
+      // console.log(`stdout: ${data}`);
+      console.log(`success`);
     });
 
     tryPasswordBruteForce.stderr.on('data', (data: any) => {
-      console.error(`stderr: ${data}`);
+      console.error(`error`);
     });
 
     tryPasswordBruteForce.on('close', (code: any) => {
-      console.log(`child process exited with code ${code}`);
+      // console.log(`child process exited with code ${code}`);
     });
   }
 
   /** End Main **/
-
-  /*
-  private assignANewValueToPasswordArrayIndex(
-    previousIndex: number, newIndex: number,
+  private chageValueOfPasswordTableIndex(
+    previousIndex: number, passLength: number, newIndex: number,
   ): void {
-    this.passwordTable[previousIndex] = this.omega[newIndex];
-  }*/
+    this.passwordTable[(passLength - previousIndex) % ( passLength + 1 )] = this.omega[newIndex];
+  }
 }
